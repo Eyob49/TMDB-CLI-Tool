1e4930ebc4a30104454fe41761ddb24886432211
Eyob49 <130957716+Eyob49@users.noreply.github.com>
2025-09-14 15:36:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ parser.add_argument("--type", "-t",type=str, help="Movie Category")
 args = parser.parse_args()
 
 api_key = os.environ["API_KEY"]
+console = Console()
 
 def get_movies(category):
-  
   VALID_CATEGORIES = {"p": "popular"
                       ,
                      "tr": "top_rated"
@@ -29,9 +29,9 @@ def get_movies(category):
      category = category
   else:
    if len(category) <= 2:
-    print(f"⚠️ Invalid category short key: {category}. Try one of {', '.join(VALID_CATEGORIES.keys())}")
+    console.print(f"⚠️ Invalid category short key: {category}. Try one of {', '.join(VALID_CATEGORIES.keys())}", style="bold red")
    else:
-     print(f"⚠️ Invalid category: {category}. Try one of {', '.join(VALID_CATEGORIES.values())}")
+     console.print(f"🚫 '{category}' is not a valid category. Try: popular, top_rated, upcoming, now_playing.", style="bold yellow")
    return
 
   
@@ -44,17 +44,23 @@ def get_movies(category):
     data = response.json()  
     
   except requests.exceptions.RequestException:
-    print("❌ Network error: please check your connection")
+    console.print("⚠️ Oops! Couldn’t reach TMDB servers. Please check your internet.", style="bold red")
     return
     
   except KeyError:
-    print("⚠️ Unexpected response format from API.")
+    console.print("⚠️ Unexpected response format from API.", style="bold red")
     return
+    
+  except Exception:
+    console.print("💥 Something went wrong! Please try again.", style="bold red")
+    return
+
     
   results = data.get("results", [])
   
   if not results:
-        print("⚠️ No movies found for this category.")
+        console.print(f"😕 No {category} movies found. Try again later!", style="italic cyan")
+
         return
 
   category_keys = {"t":"title",
@@ -67,21 +73,21 @@ def get_movies(category):
       elif user_choice in category_keys.keys():
         user_choice = category_keys.get(user_choice)
         break
-      print("Please be sure you chose title, release_date or vote_average")
+      console.print("Please be sure you've chosen title(t), release_date(d) or vote_average(r)" ,style="bold red")
    
   sorted_data = sorted(data["results"], key=lambda item: item[user_choice])
   
   def do_something_important():
     time.sleep(5.0)
     
-  console = Console()
-  with console.status(f"Please wait, fetching {category} movies", spinner="earth"):
+  
+  with console.status(f"Exploring TMDB universe for {category} movies", spinner="earth"):
     do_something_important()
-  console.print(":rocket: Here's it...\n")
+  console.print(f":rocket: All set! Here are the {category} movies\n")
                       
   
   def display_movies(movies):
-    table = Table(title=":clapper_board: Movies")
+    table = Table(title=":clapper_board: Movies", show_header=True, title_justify = "left")
     table.add_column("Title", style="cyan", no_wrap=True)
     table.add_column("Released date", style="blue")
     table.add_column("Rating", style="yellow")
@@ -95,7 +101,19 @@ def get_movies(category):
 
     console.print(table)
 
-    display_movies(sorted_data)
-
+  display_movies(sorted_data)
 
 get_movies(args.type)
+
+
+
+
+
+
+
+
+
+
+
+
+
